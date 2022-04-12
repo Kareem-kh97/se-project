228a42ff7cd99930618f545b3ad83bfb380549c8
Benjamin Pasic <benjamin.pasic@stu.ibu.edu.ba>
2022-04-13 01:27:50 +0200

    setup ejs and added landing page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+//Start server
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+});
+
+//Setup ejs as our templating language
+app.set("view engine", "ejs");
+
+//landing page route
+app.get("/", (req, res) => {
+  res.render("index");
 });
