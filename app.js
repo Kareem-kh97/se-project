1731475b49788e63fe6269060d3f6fc835d3f02f
Benjamin Pasic <benjamin.pasic@stu.ibu.edu.ba>
2022-04-22 18:56:10 +0200

    Added dynamic port for heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const userRoutes = require("./routes/userRoutes");
 app.set("view engine", "ejs");
 
 //Start server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
 
