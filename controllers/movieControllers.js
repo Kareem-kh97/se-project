7288a2d404f5983fca08c3d9e509028e5c473ec7
Benjamin Pasic <benjamin.pasic@stu.ibu.edu.ba>
2022-04-22 23:58:53 +0200

    Added authorization

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,5 +1,13 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 const Movies = require("../models/Movies");
 const ActorsMovies = require("../models/ActorsMovies");
+
+const decodeToken = async (token) => {
+  const decoded = await jwt.verify(token, process.env.SECRET);
+  return decoded;
+};
 
 const movie_get = async (req, res) => {
   const [movies] = await Movies.getMovies();
@@ -12,7 +20,19 @@ const movie_by_id_get = async (req, res) => {
   res.render("moviedetails", { movieAndActors });
 };
 
+const add_review_get = async (req, res) => {
+  const decodedToken = await decodeToken(req.cookies.jwt);
+  const [user] = await User.getById(decodedToken.id);
+
+  if (!user[0].isEditor) {
+    res.render("403");
+  }
+
+  res.render("addmoviereview");
+};
+
 module.exports = {
   movie_get,
   movie_by_id_get,
+  add_review_get,
 };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -8,4 +8,6 @@ router.get("/", authenticateUser, movieControllers.movie_get);
 
 router.get("/movie/:id", authenticateUser, movieControllers.movie_by_id_get);
 
+router.get("/addreview", authenticateUser, movieControllers.add_review_get);
+
 module.exports = router;
