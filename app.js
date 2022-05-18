ee7d6cac6c7ef3c2e4dbff04b720021bd6389df7
Benjamin Pasic <benjamin.pasic@stu.ibu.edu.ba>
2022-05-18 15:03:53 +0200

    test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const PORT = process.env.PORT || 3000;
@@ -24,6 +25,11 @@ app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 //Include routes
 app.use(userRoutes);
